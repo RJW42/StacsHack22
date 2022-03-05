f8a9dbe13232b9e6bed3a38e30fcd343cb2ad573
Robbie <robbiejohnwallace@yahoo.co.uk>
2022-03-05 12:05:57 +0000

    removed duplicate endpoint

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html'); 
-});
 
 server.listen(8081,function(){ // Listens to port 8081
     console.log('Listening on '+server.address().port);
